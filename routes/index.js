b3daa440faa902dda3288ff936250f76c12fbe0b
Joshua Kravitz <jlkravitz@gmail.com>
2013-10-17 18:30:31 -0700

    basics of share link page -- set up link/code generator

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,25 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'QuickPolls' });
+	res.render('index', { title: 'QuickPolls' });
+};
+
+exports.share = function(req, res){
+		var question = req.body.question;
+		delete req.body.question;
+
+		var choices = [];
+		for (var param in req.body) {
+			if (req.body.hasOwnProperty(param)) {
+				choices.push(req.body[param]);
+			}
+		}
+
+		var PollProvider = new (require('./pollprovider').PollProvider);
+		PollProvider.save({
+			question: question,
+			choices: choices
+		}, function(error, pollCode){
+			res.render('share', { title: 'Share your poll', question: question, choices: choices, code: pollCode });
+		});
 };
---
+++ routes/pollprovider.js
@@ -0,0 +1,33 @@
+PollProvider = function(){
+
+};
+
+PollProvider.prototype.polls = {};
+
+PollProvider.prototype.findByCode = function(code, callback){
+	var result = polls[code] || null;
+	callback(null, result);
+}
+
+PollProvider.prototype.save = function(poll, callback){
+	console.log('Saving...');
+	var code = generateCode.call(this);
+	console.log('Generated code...' + code);
+	this.polls[code] = poll;
+	console.log('About to call callback...');
+	callback(null, code);
+
+	function generateCode(){
+		var code;
+	    while (true){
+		    code = Math.random().toString(36).substr(2); // remove '0.'
+		    if (!this.polls[code]){
+		    	break;
+		    }
+	    }
+	    
+	    return code;
+	};
+};
+
+exports.PollProvider = PollProvider;
