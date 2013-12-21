fa45bcf901874682f0dc5db69489dac0310710ba
Joshua Kravitz <jlkravitz@gmail.com>
2013-12-21 11:48:14 -0800

    move some js to separate file

---
+++ public/js/main.js
@@ -0,0 +1,93 @@
+var QuestionCreator = (function() {
+	// id of current choice (only has to be unique, not consecutive)
+	var currentId = 1;
+
+	var setup = function() {
+		appendNewChoice();
+		appendNewChoice();
+		appendNewChoice();
+
+		$('#addPollChoice').on('click', function(e) {
+			appendNewChoice();
+		});
+
+		$(document).on('click', '.closePollChoice', function(e) {
+			$(this).parent().remove();
+		});
+
+		$('form').on('submit', function() {
+			/* Check for question */
+			var questionFormGroup = $(this).children().first();
+			if ($('#newPollQuestion').val() === '') {
+				questionFormGroup.addClass('has-error');
+				return false;
+			} else {
+				questionFormGroup.removeClass('has-error');
+			}
+
+			/* Remove unused choices */
+			var choices = [];
+			$('.newPollChoice').each(function(index, choice) {
+				if (choice.value === '') {
+					$(this).parent().remove();
+				}
+			});
+
+			/* Check for MIN_CHOICES choices. */
+			var MIN_CHOICES = 2;
+			var completedChoices = $('.newPollChoice').length;
+			if (completedChoices < MIN_CHOICES) {
+				for (var i = 0; i < MIN_CHOICES - completedChoices; i++){
+					appendNewChoice();
+				}
+				$('.newPollChoice').each(function(index, choice) {
+					if (choice.value === '') {
+						$(this).parent().addClass('has-error');
+					} else {
+						$(this).parent().removeClass('has-error');
+					}
+				});
+				return false;
+			}
+
+			return true;
+		});
+	};
+
+	var appendNewChoice = function() {
+		var choiceHtml = $('#newPollChoiceTemplate').html();
+		choiceHtml = choiceHtml.replace(/{{id}}/g, currentId.toString());
+		$('#poll-choices').first().append(choiceHtml);
+		++currentId;
+	}
+
+	return {
+		setup: setup
+	};
+})();
+
+var Results = (function() {
+	var getMaxVotes = function() {
+		var max = 0;
+		$('.progressBar').each(function(i, elem){
+			var votes = parseInt($(elem).data('votes'));
+			if (votes > max){
+				max = votes;
+			}
+		});
+		return max;
+	}
+
+	var setup = function() {
+		$('.progressBar').tooltip();
+		var maxVotes = getMaxVotes();
+		$('.progressBar').each(function(i, elem){
+			var votes = parseInt($(elem).data('votes'));
+			$(this).width(votes / maxVotes * 100);
+		});
+	};
+
+	return {
+		setup: setup
+	};
+})();
